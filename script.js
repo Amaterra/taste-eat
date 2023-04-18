0fe00ab23db3c6a28608f53091b7787152021f33
Amaterra <ami.less@yandex.ru>
2023-04-18 22:13:00 +0300

    burger menu

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ if (window.screen.width >= 540) {
    window.addEventListener('resize', init);
    init();
 
+   //btn slide next 
 
    document.querySelector('.section-five__sider-button-next').addEventListener('click', function () {
       count++;
@@ -29,15 +30,9 @@ if (window.screen.width >= 540) {
       function rollSlider() {
          sliderLine.style.transform = 'translate(-' + count * width + 'px)';
       }
-
-
    })
 
-
-   function slidePrev1() {
-      console.log('slevaNaPravo');
-   }
-
+   //btn slide prev
 
    document.querySelector('.section-five__sider-button-prev').addEventListener('click', function () {
       count--;
@@ -50,12 +45,9 @@ if (window.screen.width >= 540) {
    function rollSlider() {
       sliderLine.style.transform = 'translate(-' + count * width + 'px)';
    }
-
 }
 
-
-
-//540
+//<= 540px width screen
 
 const sliderCardOne = document.querySelectorAll('.section-five__slider_one');
 
@@ -94,10 +86,9 @@ if (window.screen.width <= 539) {
    }
 }
 
-
+//swipe
 
 function slideNextSwipe() {
-
    count++;
    if (count >= sliderCard.length) {
       count = 0;
@@ -107,8 +98,6 @@ function slideNextSwipe() {
    function rollSlider() {
       sliderLine.style.transform = 'translate(-' + count * width + 'px)';
    }
-
-   console.log('12');
 }
 
 function slidePrevSwipe() {
@@ -118,54 +107,10 @@ function slidePrevSwipe() {
    }
    rollSlider();
 
-
    function rollSlider() {
       sliderLine.style.transform = 'translate(-' + count * width + 'px)';
    }
 }
-
-
-
-
-
-
-// var touchstartX = 0;
-// var touchstartY = 0;
-// var touchendX = 0;
-// var touchendY = 0;
-
-// var gesuredZone = document.getElementById('slider-cards');
-
-// gesuredZone.addEventListener('touchstart', function (event) {
-//    touchstartX = event.screenX;
-//    touchstartY = event.screenY;
-// }, false);
-
-// gesuredZone.addEventListener('touchend', function (event) {
-//    touchendX = event.screenX;
-//    touchendY = event.screenY;
-//    handleGesure();
-// }, false);
-
-// function handleGesure() {
-//    // var swiped = 'swiped: ';
-//    if (touchendX < touchstartX) {
-//       console.log('prprpr');
-//    }
-//    if (touchendX > touchstartX) {
-//       console.log('plplpl');
-//    }
-//    if (touchendY < touchstartY) {
-//       console.log('p2prpr');
-//    }
-//    if (touchendY > touchstartY) {
-//       console.log('p7prpr');
-//    }
-//    if (touchendY == touchstartY) {
-//       slideNext12();
-//    }
-// }
-
 
 document.addEventListener('touchstart', handleTouchStart, false);
 document.addEventListener('touchmove', handleTouchMove, false);
@@ -191,7 +136,7 @@ function handleTouchMove(event) {
    let yDiff = y2 - y1;
 
    if (Math.abs(xDiff) > Math.abs(yDiff)) {
-      //r-l
+
       if (xDiff > 0) {
          slidePrevSwipe();
       }
@@ -205,48 +150,14 @@ function handleTouchMove(event) {
    y1 = null;
 }
 
+// burger menu
 
+const tabBurgerMenu = document.querySelector(".head__burger");
+const tabNavigation = document.querySelector(".burger-menu");
+const bodyLock = document.querySelector(".body");
 
-
-
-
-
-
-// let slideIndex = 0;
-
-// let slides = document.getElementsByClassName(".section-five__slider_one");
-// let sliderContainer = document.getElementsByClassName(".slider__slider-line")[0];
-
-// // if (typeof sliderContainer !== 'undefined') {
-// // };
-
-
-// let touchStartX = 0;
-// let touchEndX = 0;
-
-// let MIN_SWIPE_DISTANCE = 50; // Minimum distance to trigger a swipe
-
-// function showSlide(n) {
-//    slides[slideIndex].style.display = "none";
-//    slideIndex = (n + slides.length) % slides.length;
-//    slides[slideIndex].style.display = "block";
-// }
-
-// Swipe gesture handling
-
-// sliderContainer.addEventListener("touchstart", function (event) {
-//    touchStartX = event.touches[0].clientX;
-// });
-
-// sliderContainer.addEventListener("touchend", function (event) {
-//    touchEndX = event.changedTouches[0].clientX;
-//    let swipeDistance = touchEndX - touchStartX;
-
-//    if (swipeDistance > MIN_SWIPE_DISTANCE) {
-//       // Swipe left to right
-//       showSlide(-1);
-//    } else if (swipeDistance < -MIN_SWIPE_DISTANCE) {
-//       // Swipe right to left
-//       showSlide(1);
-//    }
-// });
+tabBurgerMenu.addEventListener("click", function () {
+   tabBurgerMenu.classList.toggle("active");
+   tabNavigation.classList.toggle("active");
+   bodyLock.classList.toggle("lock");
+});
